87af626e70c6b75ae1f0a0f25a55a9d50062cccc
userbyte <42632711+userbyte@users.noreply.github.com>
2022-01-19 08:01:37 -0500

    Initial commit

---
+++ bash_historian.py
@@ -0,0 +1,9 @@
+## bash_historian
+## 1/19/2022
+## Written by userbyte
+## Description: Keeps track of the .bash_history file. Generates stats about most used commands, commonly misspelled commands, etc. Runs periodically to create over time statistics.
+
+import os
+
+currentuser = str(os.getlogin())
+histfile = f"/home/{currentuser}/.bash_history"
